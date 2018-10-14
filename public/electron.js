a49701eb1fe8e5b253f73114d73ee23e0cb42abe
V1shvesh <22243137+V1shvesh@users.noreply.github.com>
2018-10-14 11:09:42 +0530

    Setup project

---
+++ public/electron.js
@@ -0,0 +1,41 @@
+const electron = require('electron');
+
+const { app, BrowserWindow, ipcMain } = electron;
+
+const { exec } = require('child_process');
+const path = require('path');
+const isDev = require('electron-is-dev');
+
+let mainWindow;
+
+ipcMain.on('runPy', (event) => {
+  exec('echo hello', (err, stdout) => {
+    if (err) {
+      event.returnValue = err;
+    } else {
+      event.returnValue = stdout;
+    }
+  });
+});
+
+function createWindow() {
+  mainWindow = new BrowserWindow({ width: 1080, height: 720 });
+  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const { ipcRenderer } = window.require('electron');
+
 class App extends Component {
+  runPy = () => {
+    console.log(ipcRenderer.sendSync('runPy'));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <button type="button" className="start-button" onClick={this.runPy}>Start VA</button>
       </div>
     );
   }
